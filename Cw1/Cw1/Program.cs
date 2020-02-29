71e596cea6155d1afa1591f2aa94c9a10cc9f03a
Piotr Kwiatek <s18033@pjwstk.edu.pl>
2020-02-29 17:49:43 +0100

    End cw1

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static async Task<HttpResponseMessage> getPageContent(string url) {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            return response;
         }
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var task = await getPageContent(args[0]);
+                var str = getEmails(task.Content.ReadAsStringAsync());
+                Console.WriteLine("==== Result ==========");
+                Console.WriteLine(str);
+                Console.WriteLine("==== End of result ===");
+            } catch(Exception e)
+            {
+                Console.WriteLine("Address Incorrect", e);
+            }
+        }
+
+        public static string getEmails(Task<string> content)
+        {
+            Regex reg = new Regex(@"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}", RegexOptions.IgnoreCase);
+            Match match;
+            string result = "";
+
+
+            for (match = reg.Match(content.Result); match.Success; match = match.NextMatch())
+            {
+                if (!result.Contains(match.Value))
+                {
+                    result += match.Value + " ";
+                }
+            }
+
+            return result;
+        } 
     }
 }
